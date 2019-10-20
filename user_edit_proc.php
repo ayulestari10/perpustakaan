712cb2c40a400b37859a01d385055cc02774f424
adesyudhatama <adesyudhatama@gmail.com>
2019-10-20 21:02:27 +0700

    query edit user

--- user_edit_proc.php
+++ user_edit_proc.php
@@ -8,10 +8,13 @@
     $email      = $_POST['email'];
     $password   = $_POST['password'];
 
-    $sql    = "update user 
-    set id_user = $id_user, username = '$username', name = '$name', 
-    email = '$email', password = '$password'
-    where id_user = $id_user";
+    $sql    ="update user
+    set id_user = ". $id_user .",
+    username = '". $username ."',
+    name = '". $name ."', 
+    email = '". $email ."',
+    password = '". $password ."'
+    where id_user = ". $id_user;
     $simpan = $db->query($sql);
 
     $_SESSION['msg_success'] = "<br><b>Data Telah Di Update</b><br>";
